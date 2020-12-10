14b5d8ef08cd6c1088019b6b324a7dda4957927d
Goshagosha <svezhegor@gmail.com>
2020-12-10 18:48:15 +0100

    uh

--- status-effect-timer.js
+++ status-effect-timer.js
@@ -1,11 +1,33 @@
-const placeholder = function(context) {
-	alert("place is held!");
+const setTimer = function(t) {
+    alert("chosen");
 }
 
-const CustomActivateListeners = function(html) {
-	TokenHUD.prototype.activateListeners(html);
-	//html.find(".status-effects")
-	//	.on("dblclick", ".effect-control", this.placeholder.bind(this));
+const popDialog = function(){
+
+	new Dialog({
+		title: "Select duration (in rounds)",
+		buttons: {
+			one: {
+				label: "One",
+				callback: (html) => {
+					setTimer(2);
+				}
+			},
+			ten: {
+				label: "Ten",
+				callback: (html) => {
+					setTimer(10);
+				}
+			},
+			hundred: {
+				label: "Hundred",
+				callback: (html) => {
+					setTimer(100);
+				}
+			}
+		},
+		default: "one"    
+	}).render(true)
 }
 
 Hooks.on("ready", function() {
