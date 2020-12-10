0b817890b7f379221329272974b006b73fdc2a52
Goshagosha <svezhegor@gmail.com>
2020-12-10 17:06:21 +0100

    tmp

--- status-effect-timer.js
+++ status-effect-timer.js
@@ -6,15 +6,13 @@ const placeholder = function(context) {
 	alert("place is held!");
 }
 
-class ExtHud extends TokenHUD {
-	activateListeners(html) {
-		super.activateListeners(html);
-		html.find(".status-effects")
-			.on("dblclick", ".effect-control", this.placeholder.bind(this));
-	}
+CustomActivateListeners = function(html) {
+	super.activateListeners(html);
+	html.find(".status-effects")
+		.on("dblclick", ".effect-control", this.placeholder.bind(this));
 }
 
 Hooks.on("ready", function() {
 	tmp_d("Hook activated");
-	TokenHUD = ExtHud;
+	TokenHUD.prototype.activateListeners = CustomActivateListeners;
 });
