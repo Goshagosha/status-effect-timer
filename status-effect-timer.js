aaf19bfd2c25e30768470d3ceda044f1fcab1aef
Goshagosha <svezhegor@gmail.com>
2020-12-10 15:54:46 +0100

    modified:   status-effect-timer.js

--- status-effect-timer.js
+++ status-effect-timer.js
@@ -4,23 +4,20 @@ const d = function(message) {
 	console.log("Status effect timer | " + message)
 }
 
-const Simple5econs = (() => {
-
-	const timerSelection = function(context) {
-		alert("HELLO WORLD!");
-	}
+const timerSelection = function(context) {
+	alert("HELLO WORLD!");
+}
 
 
-	const overrideOnClickListener = function(html) {
-		d("overide working");
-		TokenHUD.activateListeners(html);
-		html.find(".status-effects")
-			.on("dblclick", ".effect-control", timerSelection.bind(this));
-	};
-	
-  // HOOKS  
-  Hooks.on("ready", function() {
-		d("Hook activated");
-		TokenHUD.activateListeners = overrideOnClickListener;
-  });
-})();
+const overrideOnClickListener = function(html) {
+	d("overide working");
+	TokenHUD.activateListeners(html);
+	html.find(".status-effects")
+		.on("dblclick", ".effect-control", timerSelection.bind(this));
+};
+
+// HOOKS  
+Hooks.on("ready", function() {
+	d("Hook activated");
+	TokenHUD.activateListeners = overrideOnClickListener;
+});
